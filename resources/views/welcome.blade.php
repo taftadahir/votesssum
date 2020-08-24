02f38fd7051bb78db77d46fd634c3849a6030822
taftadjani <taftadjani@gmail.com>
2020-08-24 15:07:32 +0200

    third commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel deployment on heroku
                 </div>
 
                 <div class="links">
